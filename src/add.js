8a3a3897304619574d76dd7047bc2e2d05855388
Kyla Allen <themightyflorp@kylas-air.attlocal.net>
2018-11-04 15:22:08 -0600

    got the add movie funtionality up and running!

--- src/add.js
+++ src/add.js
@@ -2,17 +2,37 @@
 
 
 
-export const options = {
-"method": "POST",
-"headers": {
-    "Content-Type": "application/json",
-},
-body: JSON.stringify(addMovie)
+export const getRating = (stars) => {
+    switch (stars) {
+        case "*":
+            return 1;
+        case "**":
+            return 2;
+        case "***":
+            return 3;
+        case "****":
+            return 4;
+        case "*****":
+            return 5;
+        default:
+            return "undefined"
+    }
 };
+//TODO: Need to access the JSON file,
+//make the file workable
+// then access the array in the JSON file
+//then push the newMovie object onto that array
+//then parse the array back into JSON
+export const addMovie = ({title, rating}) => {
+    let newMovie = { title, rating };
+    fetch('./api/movies', {
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json"},
+        body: JSON.stringify(newMovie)
 
-
-export const addMovie = (newMovie) => {
-    return fetch('./api/movies', options)
-        .then(response => response.json());
+    })
+        .then(response => JSON.stringify(response))
+        .then(data => console.log(data));
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 /**
  * es6 modules and imports
  */
-import { getMovies } from './api.js';
-import sayHello from './hello';
-import { buildHTML } from './build-html';
 import $ from "jquery";
+import { getMovies } from './api.js';
+import { buildHTML } from './build-html';
 import { addMovie } from "./add";
-import { options } from "./add";
+import { getRating } from "./add";
 
 //snagging html elements
 let movieHtml = $(".movies");
@@ -14,27 +13,32 @@ let addButton = $("#submitMovie");
 
 
 //display movies
-getMovies().then((movies) => {
-  let html = "";
-  movies.forEach(({title, rating, id}) => {
-    // console.log(`id#${id} - ${title} - rating: ${rating}`);
-      html += buildHTML(title,rating,id);
-  });
-  movieHtml.html(html);
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+const loadMovies= () => {
+    getMovies().then((movies) => {
+        let html = "";
+        movies.forEach(({title, rating, id}) => {
+            // console.log(`id#${id} - ${title} - rating: ${rating}`);
+            html += buildHTML(title, rating, id);
+        });
+        movieHtml.html(html);
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
+    });
+}
 
+loadMovies();
 
 //add movies
 addButton.on('click', function (e) {
   e.preventDefault();
+  let rating = getRating($('#rating').val());
   const newMovie = {
       "title": $('#title').val(),
-    "rating": $('#rating').val()
+      "rating": rating
   };
    addMovie(newMovie);
+   loadMovies();
 });
 
 
