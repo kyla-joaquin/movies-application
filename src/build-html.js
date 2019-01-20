71cc81fefdade93f0e4e0114f6020ecd4c9a9b01
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2019-01-20 01:02:19 -0600

    refactored the functions to not show the id to the user when each movie card is rendered. Found a better image background for the movie cards. Added titles to the add movie side box and the edit movie side box.

--- src/build-html.js
+++ src/build-html.js
@@ -2,12 +2,12 @@
 
 export const buildHTML = (title, rating, id) => {
     let html = `<div class="card carousel-item">`;
-    html += `<img class="card-img-top" src="https://picsum.photos/200/300">`;
+    html += `<img class="card-img-top" src="img/fancycrave-440143-unsplash.jpg">`;
     html += `<div class="card-body">`;
     html += `<h4 class="card-title"> ${title} </h4>`;
-    html += `<p class="card-text"> Rated: ${rating}, ID: <span class="id">${id}</span></p>`;
-    html += `<button class="deleteBtn"> Delete </button>`;
-    html += `<button class="editBtn"> Edit </button>`;
+    html += `<p class="card-text"> Rated: ${rating}</p>`;
+    html += `<button class="deleteBtn" id="${id}"> Delete </button>`;
+    html += `<button class="editBtn" id="${id}"> Edit </button>`;
     html += `</div>`;
     html += `</div>`;
     return html;
